ee966636abc7b4cef82f138785cd6fc4f0abc26b
jmarino <jim.marino@gmail.com>
2014-10-31 15:04:11 +0100

    Try rest annotations

--- src/main/java/com/metaformsystems/integral/test/spring/AccountService.java
+++ src/main/java/com/metaformsystems/integral/test/spring/AccountService.java
@@ -1,7 +1,19 @@
 package com.metaformsystems.integral.test.spring;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  */
+@RestController
+@RequestMapping("/service/account")
 public class AccountService {
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String getAccount(@PathVariable String id) {
+        return "";
+    }
 }
