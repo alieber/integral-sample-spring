89c8318bed679fd63b4420e1a7786c8a26145d46
jmarino <jim.marino@gmail.com>
2014-10-31 15:59:47 +0100

    Add JAX-RS client

--- src/main/java/com/metaformsystems/integral/test/spring/AccountService.java
+++ src/main/java/com/metaformsystems/integral/test/spring/AccountService.java
@@ -1,5 +1,11 @@
 package com.metaformsystems.integral.test.spring;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,8 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/service/account")
 public class AccountService {
 
+    private static final String TARGET = "http://foo.com/service";
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public String getAccount(@PathVariable String id) {
+        Client client = ClientBuilder.newBuilder().build();
+        WebTarget target = client.target(TARGET);
+        Invocation.Builder request = target.request();
+        Response response = request.get();
         return "";
     }
 }
